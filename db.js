a4346dc6ca252186200f082febfc17cc6da8e09c
ZenzeroBro <165959392+ZenzeroBro@users.noreply.github.com>
2025-11-19 20:36:06 -0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",   // empty cuz we didn't set one 
+  database: process.env.DB_DATABASE || "ecoWattch_db",
+  port: process.env.DB_PORT || 3306,        // XAMPP MySQL port from configuration
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
